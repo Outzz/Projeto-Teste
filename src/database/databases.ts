48a31f2f9d0089d5da2126e972a20aa27913d86f
Outzz <blueoutz.kka@gmail.com>
2026-03-24 21:43:16 -0300

    atualizacoes da biblioteca sqlite3

--- src/database/databases.ts
+++ src/database/databases.ts
@@ -1,9 +1,11 @@
 import Database from "better-sqlite3";
 import path from "path";
 import fs from "fs";
+
 const dbPath = path.resolve(__dirname, "../../database.db");
 const schemaPath = path.resolve(__dirname, "./schema.sql");
 const db = new Database(dbPath);
 const schema = fs.readFileSync(schemaPath, "utf-8");
+
 db.exec(schema);
 export default db;
